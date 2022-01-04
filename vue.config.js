49f571ff1f612226bdea4a271dc90311906caaa8
arunkumarsubburaj <arun@appsmav.com>
2022-01-04 08:18:20 +0530

    Updating dist files name

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,10 @@ module.exports = {
     optimization: {
       splitChunks: false,
     },
+    // Naming bundle 'email_templates_bo'
+    entry: {
+      embed_bo: "./src/main.js",
+    },
   },
   // css: {
   //     extract: false,
